565d6683ed71c1a4fea29d64bbb1950e03a24e2a
igorbs <113237330+Igorbolanel@users.noreply.github.com>
2026-04-19 18:46:52 +0300

    Delete lab3-tests directory

--- lab3-tests/src/main/java/com/bolanel/labs/exception/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bolanel.labs.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
